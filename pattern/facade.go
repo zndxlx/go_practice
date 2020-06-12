1a912ef511c945bf7e2c042fec4d9f7732ea12ce
lixin <lixing@hpplay.cn>
2020-06-12 08:43:35 +0800

    111111111111

---
+++ pattern/facade.go
@@ -0,0 +1,47 @@
+package main
+
+type Music struct {
+    Name string
+}
+
+func (m *Music) GetMusic() string {
+    return m.Name
+}
+
+type Video struct {
+    Id int64
+}
+
+func (v *Video) GetVideoId() int64 {
+    return v.Id
+}
+
+type Count struct {
+    Comment int64
+    Praise int64
+    Collect int64
+}
+
+func (c *Count)GetComment()int64{
+    return c.Comment
+}
+
+type Facade struct{
+    music Music
+    count Count
+    video Video
+}
+
+func (f Facade)PrintServerInfo(){
+    f.music.GetMusic()
+    f.video.GetVideoId()
+    f.count.GetComment()
+}
+
+func NewFacade(music Music, count Count, video Video)*Facade{
+    return &Facade{
+        music:music,
+        video:video,
+        count:count,
+}
+}
